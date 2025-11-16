bc9118e4234249daf44eea9f7c63a6f913dbe407
zhangdahai112 <631871336@qq.com>
2025-11-16 23:51:42 +0800

    elastic-job

--- server/src/main/java/com/server/server/ServerApplication.java
+++ server/src/main/java/com/server/server/ServerApplication.java
@@ -1,6 +1,12 @@
 package com.server.server;
 
-import org.springframework.boot.SpringApplication;
+import org.apache.shardingsphere.elasticjob.api.JobConfiguration;
+import org.apache.shardingsphere.elasticjob.api.ShardingContext;
+import org.apache.shardingsphere.elasticjob.lite.api.bootstrap.impl.ScheduleJobBootstrap;
+import org.apache.shardingsphere.elasticjob.reg.base.CoordinatorRegistryCenter;
+import org.apache.shardingsphere.elasticjob.reg.zookeeper.ZookeeperConfiguration;
+import org.apache.shardingsphere.elasticjob.reg.zookeeper.ZookeeperRegistryCenter;
+import org.apache.shardingsphere.elasticjob.simple.job.SimpleJob;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
@@ -9,7 +15,34 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 public class ServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServerApplication.class, args);
+//        SpringApplication.run(ServerApplication.class, args);
+
+        new ScheduleJobBootstrap(createRegistryCenter(), new MyJob(), createJobConfiguration()).schedule();
     }
 
+    private static CoordinatorRegistryCenter createRegistryCenter() {
+        CoordinatorRegistryCenter regCenter = new ZookeeperRegistryCenter(new ZookeeperConfiguration("47.121.183.184:6181", "my-job"));
+        regCenter.init();
+        return regCenter;
+    }
+
+    private static JobConfiguration createJobConfiguration() {
+        // 创建作业配置
+        // ...
+        return JobConfiguration.newBuilder("MyJob", 3).cron("0/5 * * * * ?").build();
+    }
+
+}
+
+class MyJob implements SimpleJob {
+
+    @Override
+    public void execute(ShardingContext context) {
+        switch (context.getShardingItem()) {
+            default -> {
+                System.out.println("Executing shard: " + context.getShardingItem());
+            }
+            // case n: ...
+        }
+    }
 }
